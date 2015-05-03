a7e330036c7fa7caa933174f59ffeb95f31431a8
Uri Mikhli <urimikhli@gmail.com>
2015-05-03 16:33:25 -0400

    WIP: use file extension to figure out how to read the file

--- inventory.rb
+++ inventory.rb
@@ -1,6 +1,6 @@
 #the inventory
 class Inventory
-  attr_accessor @records
+  attr_accessor :records
 
   INVETORYSTORE = 'db/inventory.json'
 
@@ -22,7 +22,41 @@ class Inventory
   def load_file(filename)
     raise "no file found " unless File.exist?(filename)
     #figure out file extension , load appropriate class, import the file
-    #@records = ...
+    @records = Klass_for(filename).import_file(filename)
+
   end
 
+  def Klass_for(filename)
+    case filename
+    when /\.pipe$/.match(filename)
+      PipeFile
+    when /\.csv$/.match(filename)
+      CsvFile
+    when /\.json/.match(filename)
+      JsonFile
+    else
+      raise "#{filename} does not have a recognized file type"
+    end
+  end
+
+  class JsonFile
+    def self.import_file(filename)
+    end
+  end
+
+  class CsvFile
+    def self.import_file(filename)
+      records = []
+      csv_headers = %w('artist','title','format','year')
+      raise "no file found " unless File.exist?(filename)
+      CSV.foreach(filename, :headers => csv_headers) do |row|
+        records.push row
+      end
+    end
+  end
+
+  class PipeFile
+    def self.import_file(filename)
+    end
+  end
 end
