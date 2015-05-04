b7541fe2ad49b6b67d5c298afe527a3d8869c464
Uri Mikhli <urimikhli@gmail.com>
2015-05-04 02:22:17 -0400

    WIP: converting CSVRow to Inventory hash TODO: This is horrible code, needs to be refactored

--- inventory.rb
+++ inventory.rb
@@ -63,7 +63,7 @@ class Inventory
   end
 
   class CsvFile
-    attr_accessor :records
+    attr_accessor :records, :uids
     def initialize(filename)
       import_file(filename)
     end
@@ -72,9 +72,56 @@ class Inventory
       records = []
       csv_headers = %w('artist' 'title' 'format' 'year')
       CSV.foreach(filename, :headers => csv_headers) do |row|
-        records.push row
+        puts row.inspect
+        convert_to_inventory(row,records)
       end
       records
+    end
+    private
+    def convert_to_inventory(row, records)
+      uid = row["'artist'"].to_s + row["'title'"].to_s + row["'year'"].to_s
+      if uids.select{|x| x.match(uid)} #does this record exist yet
+        self.records.each do |record|
+          if record["uid"].match(uid) #find the record we are intersted in
+            if record["formats"].select{|x| x.format.match(row["format"])} #does the format already exist
+              record["formats"].each do |format|
+                if format["format"].match(row["format"]) #find format we are intersted in
+                  format["quantity"] += 1 #increment quantity
+                  break
+                end
+              end
+            else #new format for this item
+              record["formats"].push(
+                  {
+                  "uid":  uid + row["format"].to_s,
+                  "format": row["format"],
+                  "quantity": 1
+                  })
+            end
+            break
+          end
+        end
+      else #create the record
+        uids.push(row["artist"] + row["title"] + row["year"] + row["format"])
+        records.push (
+        {
+          "uid": row["artist"] + row["title"] + row["year"]
+          "artist": row["artist"],
+          "title": row["title"],
+          "year": row["year"],
+          "formats": [
+            {
+              "uid":  row["artist"] + row["title"] + row["year"] + row["format"],
+              "format": row["format"],
+              "quantity": 1
+            }
+          ]
+        }
+        )
+
+      end
+
+
     end
   end
 
